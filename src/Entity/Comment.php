c8792f908cf1760dfa8f756d27fd23ea8f8ed60b
OMAR SOTILLO FRANCO <i32sofro@uco.es>
2017-09-27 17:01:09 +0200

    User and comment related

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -12,12 +12,14 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\CommentBundle\Entity\Comment as BaseComment;
+use FOS\CommentBundle\Model\SignedCommentInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity
  * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
  */
-class Comment extends BaseComment
+class Comment extends BaseComment implements SignedCommentInterface
 {
     /**
      * @ORM\Id
@@ -33,4 +35,31 @@ class Comment extends BaseComment
      * @ORM\ManyToOne(targetEntity="App\Entity\Thread")
      */
     protected $thread;
+
+    /**
+     * Author of the comment
+     *
+     * @ORM\ManyToOne(targetEntity="MyProject\MyBundle\Entity\User")
+     * @var User
+     */
+    protected $author;
+
+    public function setAuthor(UserInterface $author)
+    {
+        $this->author = $author;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function getAuthorName()
+    {
+        if (null === $this->getAuthor()) {
+            return 'Anonymous';
+        }
+
+        return $this->getAuthor()->getUsername();
+    }
 }
