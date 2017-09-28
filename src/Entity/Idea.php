7d019f96690347961328f29a9d1f1754f1a5bc28
Sergio Gomez <sergio@uco.es>
2017-09-28 19:22:14 +0200

    Use card ui for ideas

--- src/Entity/Idea.php
+++ src/Entity/Idea.php
@@ -12,6 +12,7 @@
 namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -310,9 +311,9 @@ class Idea
     }
 
     /**
-     * @return ArrayCollection|Vote[]
+     * @return Collection|Vote[]
      */
-    public function getVotes(): ArrayCollection
+    public function getVotes(): Collection
     {
         return $this->votes;
     }
