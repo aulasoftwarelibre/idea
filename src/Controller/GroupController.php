c838a3718d8d71cfc79820a46d9d10945bd0e911
OMAR SOTILLO FRANCO <i32sofro@uco.es>
2017-09-29 15:36:27 +0200

    Group controller file created

---
+++ src/Controller/GroupController.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the ceo project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GroupController extends Controller
+{
+}
