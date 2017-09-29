c479ee94b6e3f486f882695c1353beec215ce8c6
Sergio Gomez <sergio@uco.es>
2017-09-29 17:05:07 +0200

    Join-unjoin idea button

--- src/Twig/Extension/CeoExtension.php
+++ src/Twig/Extension/CeoExtension.php
@@ -13,7 +13,10 @@ namespace App\Twig\Extension;
 
 use App\Entity\Idea;
 use App\Entity\Thread;
+use App\Entity\User;
 use App\Repository\ThreadRepository;
+use App\Repository\VoteRepository;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Translation\TranslatorInterface;
 
 class CeoExtension extends \Twig_Extension
@@ -26,18 +29,47 @@ class CeoExtension extends \Twig_Extension
      * @var TranslatorInterface
      */
     private $translator;
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+    /**
+     * @var VoteRepository
+     */
+    private $voteRepository;
 
-    public function __construct(ThreadRepository $threadRepository, TranslatorInterface $translator)
-    {
+    public function __construct(
+        VoteRepository $voteRepository,
+        ThreadRepository $threadRepository,
+        TranslatorInterface $translator,
+        TokenStorageInterface $tokenStorage
+    ) {
         $this->threadRepository = $threadRepository;
         $this->translator = $translator;
+        $this->tokenStorage = $tokenStorage;
+        $this->voteRepository = $voteRepository;
     }
 
     public function getFunctions()
     {
         return [
             new \Twig_SimpleFunction('idea_count_comments', [$this, 'getIdeaCountComments']),
+            new \Twig_SimpleFunction('is_voted', [$this, 'testLoggedUserVotes']),
         ];
+    }
+
+    public function testLoggedUserVotes(Idea $idea)
+    {
+        $token = $this->tokenStorage->getToken();
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        $vote = $this->voteRepository->findOneBy(['idea' => $idea, 'user' => $user]);
+
+        return (bool) $vote;
     }
 
     public function getIdeaCountComments(Idea $idea)
