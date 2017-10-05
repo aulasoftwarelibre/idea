e98d3f173648ed46ecdf0e464edf559df528bfe4
Sergio Gómez <sergio@uco.es>
2017-10-05 18:45:39 +0200

    Added support to limit the number of users joined to the idea

---
+++ src/Exception/NoMoreSeatsLeftException.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the ceo project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Exception;
+
+class NoMoreSeatsLeftException extends \Exception
+{
+}
