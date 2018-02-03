4a0feb2b4b513c73e437df3291f10e902bb492db
Sergio Gómez <decano@gmail.com>
2018-02-03 18:56:52 +0100

    Command GenerateUserTelegramToken
    Update telegram token

---
+++ src/Command/GenerateUserTelegramTokenCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the ceo project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Command;
+
+class GenerateUserTelegramTokenCommand
+{
+    private $userId;
+
+    /**
+     * GenerateUserTelegramTokenCommand constructor.
+     */
+    public function __construct($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+}
