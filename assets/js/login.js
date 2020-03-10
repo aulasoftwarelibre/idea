ae0a84f2802ef76750d7048be71916a8da7c0791
Sergio Gómez <sergio@uco.es>
2020-03-10 16:26:44 +0100

    Changed semantic with fomantic

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,6 @@
 global.$ = global.jQuery = require('jquery');
 
-require('semantic-ui-css');
+require('fomantic-ui-css/semantic.css')
+require('../css/reponsive-semantic-ui.css');
+require('fomantic-ui-css/semantic');
 require('../css/login.css');
