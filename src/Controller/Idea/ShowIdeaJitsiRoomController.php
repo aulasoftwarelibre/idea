52ff8a134a5772cd710659d416308b912c9000f1
Sergio Gómez <sergio@uco.es>
2020-05-11 19:28:31 +0200

    Show information about jitsi rooms

---
+++ src/Controller/Idea/ShowIdeaJitsiRoomController.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the `idea` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller\Idea;
+
+use App\Entity\Idea;
+use App\MessageBus\QueryBus;
+use App\Messenger\Idea\GetIdeaJitsiRoomUrlQuery;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/idea/{slug}/jitsi", name="idea_jitsi", methods={"GET"})
+ */
+class ShowIdeaJitsiRoomController extends AbstractController
+{
+    /**
+     * @var QueryBus
+     */
+    private $queryBus;
+
+    public function __construct(QueryBus $queryBus)
+    {
+        $this->queryBus = $queryBus;
+    }
+
+    public function __invoke(Idea $idea): Response
+    {
+        if (!$idea->isOnline()) {
+            throw $this->createNotFoundException();
+        }
+
+        if ($idea->isJitsiRoomOpen()) {
+            $url = $this->queryBus->query(
+                new GetIdeaJitsiRoomUrlQuery($idea)
+            );
+
+            return $this->redirect($url);
+        }
+
+        return $this->render('frontend/idea/jitsi.html.twig', [
+            'idea' => $idea,
+        ]);
+    }
+}
---
+++ src/Messenger/Idea/GetIdeaJitsiRoomUrlHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the `idea` project.
+ *
+ * (c) Aula de Software Libre de la UCO <aulasoftwarelibre@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Messenger\Idea;
+
+use App\MessageBus\QueryHandlerInterface;
+
+final class GetIdeaJitsiRoomUrlHandler implements QueryHandlerInterface
+{
+    /**
+     * @var string
+     */
+    private $jitsiPrefix;
+
+    public function __construct(string $jitsiPrefix)
+    {
+        $this->jitsiPrefix = $jitsiPrefix;
+    }
+
+    public function __invoke(GetIdeaJitsiRoomUrlQuery $query): ?string
+    {
+        $idea = $query->getIdea();
+
+        if (!$idea->isOnline()) {
+            return null;
+        }
+
+        return sprintf('https://meet.jit.si/%s-%s', $this->jitsiPrefix, $idea->getJitsiLocatorRoom());
+    }
+}
