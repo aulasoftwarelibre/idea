dcc4427b4ddb0eda9d692784aae9bc0f6eb47e65
Sergio Gómez <sergio@uco.es>
2020-05-11 17:13:51 +0200

    [templates] Show calendar link

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,5 @@
 global.$ = global.jQuery = require('jquery');
 
 require('fomantic-ui-css/semantic.css')
-require('../css/reponsive-semantic-ui.css');
 require('fomantic-ui-css/semantic');
 require('../css/login.css');
