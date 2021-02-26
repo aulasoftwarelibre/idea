c16bd1b9a08af82c13f9f8c4585403aab86c685a
Sergio Gómez <sergio@uco.es>
2021-02-26 21:10:18 +0100

    feat(user): check if user is enabled

---
+++ src/Security/User/UserChecker.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Security\User;
+
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+final class UserChecker implements UserCheckerInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function checkPostAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->getEnabled()) {
+            throw new AccountExpiredException('Cuenta desactivada');
+        }
+    }
+}
